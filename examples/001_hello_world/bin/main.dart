73ba09e5ed8d7fe7f267a9210e935da45357529b
Ahmed Masoud <47630729+maxzod@users.noreply.github.com>
2021-04-04 12:41:30 +0200

    hello world example

--- examples/001_hello_world/bin/main.dart
+++ examples/001_hello_world/bin/main.dart
@@ -1,29 +1,12 @@
 import 'package:palace/palace.dart';
 
 Future<void> main(List<String> arguments) async {
-  /// * you can run as any normal dart app
-  /// ? or user the light house to run and watch for changes  'package:lighthouse'
-
   final palace = Palace();
-  palace.use(guard);
-  palace.use(iamNext);
-  palace.notFoundHandler = (req, res) => res.notFound(data: 'custom not found handler msg');
 
-  palace.get('/', (req, res) => res.json('Long Live The Queen !'));
-  // router.get('/', (req, res) {
-  //   res.write('Long Live The Queen !');
-  //   final file = File('');
-  //   // res.request.response.addStream(file.read)
-  // });
+  palace.get(
+    '/',
+    (req, res) => res.json('Long Live The Queen !'),
+  );
+
   await palace.openGates();
-}
-
-Future<void> guard(Request req, Response res, Function next) async {
-  print('req in');
-  await next();
-}
-
-Future<void> iamNext(Request req, Response res, Function next) async {
-  print('req in next');
-  await next();
 }
